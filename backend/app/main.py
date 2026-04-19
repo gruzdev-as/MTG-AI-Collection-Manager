cad58c8bffb2aed4450f765dc90b0fec288cb87f
Aleksandr <gruzdev-as@yandex.ru>
2026-04-19 16:18:04 +0800

    FIX: remove unnecessary files and enabled redis stream for embedding generation

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,15 @@
 import tempfile
 import time
 import uuid
-from dataclasses import asdict
 from typing import Annotated
 
 import cv2
 import numpy as np
-import redis
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 
-from backend.vision.image_processor import ImageProcesser
-from common.configs.data_schemas import CropTask, RedisConfig
-
-r = redis.Redis(**asdict(RedisConfig()))
-image_processor = ImageProcesser()
+from common.configs.constants import REDIS
+from common.configs.data_schemas import EmbeddingTask
 
 app = FastAPI()
 
@@ -29,31 +24,23 @@ async def scan_card(image: Annotated[UploadFile, File()]) -> JSONResponse:
     if frame is None:
         return JSONResponse(status_code=400, content={"error": "Invalid image"})
 
-    _, contours = image_processor.find_big_contours(frame)
+    with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as tmp_file:
+        cv2.imwrite(tmp_file.name, frame)
+        tmp_file.flush()
+        tmp_file.seek(0)
 
-    crops_created = []
-    for contour in contours:
-        crop = image_processor.crop_warp_image_from_contour(frame, contour)
+    embedding_task = EmbeddingTask(
+        frame_id=str(uuid.uuid4()),
+        image_ref=tmp_file.name,
+        created_at=time.time(),
+    )
 
-        with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as tmp_file:
-            cv2.imwrite(tmp_file.name, crop)
-            tmp_file.flush()
-            tmp_file.seek(0)
-
-        crop_task = CropTask(
-            track_id=str(uuid.uuid4()),
-            frame_id=str(uuid.uuid4()),
-            image_ref=tmp_file.name,
-            created_at=time.time(),
-        )
-
-        # r.xadd("crop_stream", crop_task.model_dump(), maxlen=100, approximate=True)
-        crops_created.append(crop_task.track_id)
+    REDIS.xadd("embedding_stream", embedding_task.model_dump(), maxlen=100, approximate=True)
 
     return JSONResponse(
         status_code=200,
         content={
-            "cards_found": len(crops_created),
-            "track_ids": crops_created,
+            "cards_found": 1,
+            "current_embedding_task_len": REDIS.xlen("embedding_stream"),
         },
     )
